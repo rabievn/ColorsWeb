75a2e8a0ef0a512ad501cdca5f4bdd4e710fc7b6
Nurmuhammad <nurmuhammad.rn@gmail.com>
2023-03-20 13:40:21 +0500

    second commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const cols = document.querySelectorAll('.col')
+
+function generateRandomColor() {
+    // RGB
+}
+
+function setRandomColors() {
+    cols.forEach(col => {
+        console.log(col);
+    })
+}
+setRandomColors()
