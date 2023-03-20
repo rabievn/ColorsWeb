fc863e64c2c4cddec22e12f3b56fc4b5bb3d271a
Nurmuhammad <nurmuhammad.rn@gmail.com>
2023-03-20 14:25:02 +0500

    change color available

--- js/script.js
+++ js/script.js
@@ -2,11 +2,25 @@ const cols = document.querySelectorAll('.col')
 
 function generateRandomColor() {
     // RGB
+
+    const hexCodes = '0123456789ABCDEF';
+    let color = ''
+    for (let i = 0; i < 6; i++) {
+        color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
+    }
+   
+    return '#' + color
 }
 
 function setRandomColors() {
     cols.forEach(col => {
-        console.log(col);
+        const text = col.querySelector('h2')
+        const color = generateRandomColor()
+
+        text.textContent = color
+
+      col.style.background = generateRandomColor()
     })
 }
 setRandomColors()
+
